45417a33ee92ff504ae5889ac125b62fd4b8284e
Meiir <a12790958@gmail.com>
2024-11-11 19:55:18 +0500

    Added js and html

---
+++ emtihan4.js
@@ -0,0 +1,39 @@
+// let season = +prompt("1-ден 12-ге дейін сан енгізіңіз")
+// if(season >=3 && season <=5){
+//     alert("Spring")
+// }else if(season >=6 && season <=8){
+//     alert("Summer")    
+// }else if(season >=9 && season <=11){
+//     alert("Autumn")
+// }else if(season == 1|| season ==2 || season ==12){
+//     alert("Winter")
+// }else{
+//     alert("Сіз қате санды енгіздіңіз")
+// }
+
+
+
+
+
+// let a = +prompt("a-ға сан бер")
+// let b = +prompt("b-ға сан бер")
+// switch (true) {
+//     case (a < b):
+//         alert("a саны b санынан кіші")
+//         break;
+//     case (a > b):
+//         alert("a саны b санынан үлкен")
+//         break;
+//     default:
+//         alert("Сандар тең")
+// } 
+// alert("Екі санның орталамасы:"+ (a + b)/2)  
+
+
+
+
+
+// let san1 = +prompt("san1 енгіз")
+// let san2 = +prompt("san2 енгіз")
+// let san3 = +prompt("san3 енгіз")
+// I don't know
